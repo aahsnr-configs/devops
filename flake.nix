039b04ce76a5043c8eeb7bddec3764f09f67a920
aahsnr-configs <ahsanur041@proton.me>
2025-10-19 22:00:09 +0600

    updated

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "A Python environment with targeted x86-64-v3 optimizations, built with GCC15";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        lib = nixpkgs.lib;
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        optimizedPkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            (final: prev:
+              lib.optionalAttrs (prev.stdenv.hostPlatform.system == "x86_64-linux") {
+                bootstrapTools = prev.bootstrapTools.overrideAttrs (old: {
+                  NIX_CFLAGS_COMPILE = "-march=x86-64-v3 -flto=auto -fprofile-use";
+                });
+              })
+          ];
+        };
+
+        # --- Component Definitions ---
+
+        pythonOptimized = (optimizedPkgs.python313.overrideAttrs (oldAttrs: {
+          hardeningDisable = [ "all" ];
+        })).override {
+          stdenv = optimizedPkgs.overrideCC optimizedPkgs.stdenv optimizedPkgs.gcc15;
+
+          enableOptimizations = true; # PGO
+          enableLTO = true;
+          reproducibleBuild = false;
+        };
+
+        pythonPackages = with pythonOptimized.pkgs; [
+          pytorch torchvision torchaudio
+          jupyterlab notebook ipykernel ipywidgets
+          numpy scipy pandas scikit-learn matplotlib seaborn
+          debugpy
+        ];
+
+        pythonDevTools = with optimizedPkgs; [
+          ruff mypy basedpyright
+        ];
+
+        devTools = with pkgs; [
+          gcc15
+          cmake
+          ninja
+          pkg-config
+          gnumake
+        ];
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            devTools           # Standard tools (including gcc15)
+            ++ pythonDevTools  # Optimized tools
+            ++ pythonPackages  # Optimized libraries
+            ++ [ pythonOptimized ]; # The PGO-optimized interpreter
+        };
+
+        packages = {
+          default = pythonOptimized.withPackages (ps: pythonPackages);
+          python = pythonOptimized;
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
